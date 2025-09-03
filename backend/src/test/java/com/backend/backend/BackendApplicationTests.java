bcc60795cb1eb8f72d96727aa718fc51fc8db629
wldnjs990 <wl990@naver.com>
2025-09-04 05:45:32 +0900

    fix(frontend) : frontend 브랜치와 병합 충돌 해결

--- backend/src/test/java/com/backend/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/backend/backend/BackendApplicationTests.java
@@ -1,12 +1,10 @@
-package com.example.hackathon;
+package com.backend.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("test")
 @SpringBootTest
-class HackathonApplicationTests {
+class BackendApplicationTests {
 
     @Test
     void contextLoads() {
